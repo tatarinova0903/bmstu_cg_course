1e308d00456e2b039e046e2c6826a74a89fc8a9c
tatarinova0903 <tatarinova0903@yandex.ru>
2022-09-11 19:08:44 +0300

    удалила лишнее

--- Итог/JAVA/cg_cource_2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.cg_cource_2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.cg_cource_2 to javafx.fxml;
-    exports com.example.cg_cource_2;
-}
